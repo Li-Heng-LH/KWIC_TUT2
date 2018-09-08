7360f73218f9f865dcac4406a9be9c2500d317a0
Li Heng <liheng6567@gmail.com>
2018-09-08 14:18:44 +0800

    Sort ignore case

--- src/KWIC.java
+++ src/KWIC.java
@@ -64,11 +64,20 @@ public class KWIC {
     }
 	
     private void sortStrings() {
-        Collections.sort(shiftedStrings);
+        Collections.sort(shiftedStrings, new SortIgnoreCase());
     }
     
     public static void main(String[] args) throws Exception {
         KWIC kwic = new KWIC();
         kwic.run();
+    }
+}
+
+
+class SortIgnoreCase implements Comparator<Object> {
+    public int compare(Object o1, Object o2) {
+        String s1 = (String) o1;
+        String s2 = (String) o2;
+        return s1.toLowerCase().compareTo(s2.toLowerCase());
     }
 }
